3feb9d7a794f2c6628b5848a2af52f860e907d36
IbnuAhsani <ibnu.muhari@gmail.com>
2019-05-02 00:38:39 +0700

    feat: separate db configuration

--- app.js
+++ app.js
@@ -2,19 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const expressHanledBars = require('express-handlebars');
 const path = require('path');
-const Sequelize = require('sequelize');
-
-const db = new Sequelize('codegig', 'postgres', 'root', {
-  host: 'localhost',
-  dialect: 'postgres',
-  operatorAliases: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-});
+const db = require('./config/database');
 
 // Test DB connection
 db.authenticate()
